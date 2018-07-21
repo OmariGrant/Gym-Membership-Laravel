3f8a31eef30f5ea7a949960fb75493023338aa2c
OGrant <ogrant92@gmail.com>
2018-07-21 22:25:43 +0100

    Set up Models & Relationship

--- app/Members.php
+++ app/Members.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Members extends Model
 {
-    //
+
+
+// The attributes that are mass assignable.
+
+protected $fillable = ['firstName'];
+protected $fillable = ['lastName'];
+protected $fillable = ['address'];
+protected $fillable = ['DOB'];
+protected $fillable = ['phone'];
+protected $fillable = ['subscription'];
+
+// Find user
+
+public function user()
+{
+  return $this->belongsTo(User::class);
+}
+
+
 }
